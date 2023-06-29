0bd3cc179b5d9f0c02cdcc7086fbf99b6407dc38
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2023-06-29 14:13:16 +0530

    user-management using context api

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,20 @@
 import { Redirect, Route } from 'react-router-dom';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
 const PrivateRoute = ({ children, ...routeProps }) => {
-  const profile = false;
-  if (!profile) {
+  const { profile, isLoading } = useProfile();
+  console.log(profile);
+
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
     return <Redirect to="/signin" />;
   }
   return <Route {...routeProps}>{children}</Route>;
